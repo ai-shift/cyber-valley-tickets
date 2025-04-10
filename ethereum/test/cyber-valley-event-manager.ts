35865269cd5f6cd6983d2f9a6fa25488bbab65a9
scipunch <scipunch@gmail.com>
2025-04-10 20:29:01 +0200

    add: Partly decompose tests

--- ethereum/test/cyber-valley-event-manager.ts
+++ ethereum/test/cyber-valley-event-manager.ts
@@ -1,5 +1,5 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { assert, expect } from "chai";
+import { assert, Assertion, expect } from "chai";
 import type { BaseContract } from "ethers";
 import hre from "hardhat";
 import {
@@ -19,8 +19,42 @@ type ContractsFixture = {
 describe("CyberValleyEventManager", () => {
   const devTeamPercentage = 10;
   const masterPercentage = 50;
+    const createEventPlaceRequest = {
+      maxTickets: 100,
+      minTickets: 50,
+      minPrice: 20,
+      minDays: 1,
+    };
 
-  async function contracts(): Promise<ContractsFixture> {
+    type CreateEventPlaceRequest = typeof createEventPlaceRequest;
+
+    const updateEventPlaceRequest = {
+      eventPlaceId: 0,
+      maxTickets: 150,
+      minTickets: 20,
+      minPrice: 30,
+      minDays: 2,
+    };
+
+    type UpdateEventPlaceRequest = typeof updateEventPlaceRequest;
+
+    const asArguments = (
+      req: CreateEventPlaceRequest | UpdateEventPlaceRequest,
+    ): integer[] => {
+      if ("eventPlaceId" in req) {
+        return [
+          req.eventPlaceId,
+          req.maxTickets,
+          req.minTickets,
+          req.minPrice,
+          req.minDays,
+        ];
+      }
+      return [req.maxTickets, req.minTickets, req.minPrice, req.minDays];
+    };
+
+
+  async function deployContract(): Promise<ContractsFixture> {
     const [owner, master, devTeam] = await hre.ethers.getSigners();
     const sex = await hre.ethers.deployContract("SimpleERC20Xylose");
     const CyberValleyEventManagerFactory = await hre.ethers.getContractFactory(
@@ -36,42 +70,52 @@ describe("CyberValleyEventManager", () => {
     return { sex, eventManager, owner, master, devTeam };
   }
 
-  describe("createEventPlace", () => {
-    const createEventPlaceRequest = {
-      maxTickets: 100,
-      minTickets: 50,
-      minPrice: 20,
-      minDays: 1,
-    };
+  async function createEventPlace(request) {
+    const { eventManager, master } = await loadFixture(deployContract);
+    return await eventManager.connect(master).createEventPlace();
+  }
 
-    type CreateEventPlaceRequest = typeof createEventPlaceRequest;
+  async function createValidEventPlace() {
+    return await createEventPlace(...asArguments(createEventPlaceRequest));
+  }
 
-    const asArguments = (
-      req: CreateEventPlaceRequest,
-    ): [integer, integer, integer, integer] => {
-      return [req.maxTickets, req.minTickets, req.minPrice, req.minDays];
-    };
+  async function updateExistingEventPlace() {
+    const tx = await createValidEventPlace();
+    const receipt = await tx.wait();
+    const event = receipt.events.find(
+      (e) => e.event === "NewEventPlaceAvailable",
+    );
+    return updateEventPlace(
+      ...asArguments({
+        ...updateEventPlaceRequest,
+        eventPlaceId: event.args.eventPlaceId,
+      }),
+    );
+  }
 
+  async function expectOnlyMaster(tx) {
+    await expect(tx).to.be.revertedWith("Must have master role");
+  }
+
+  describe("EventPlace", () => {
     it("allowed only to master", async () => {
-      const { eventManager, master } = await loadFixture(contracts);
-      await expect(
+      const { eventManager } = await loadFixture(deployContract);
+      await expectOnlyMaster(
         eventManager.createEventPlace(...asArguments(createEventPlaceRequest)),
-      ).to.be.revertedWith("Must have master role");
+      );
     });
 
-    it("should emit NewEventPlaceAvailable event", async () => {
-      const { eventManager, master } = await loadFixture(contracts);
-      await expect(
-        eventManager
-          .connect(master)
-          .createEventPlace(...asArguments(createEventPlaceRequest)),
-      )
+    it("should emit events", async () => {
+      const { eventManager, master } = await loadFixture(deployContract);
+      const createEventPlaceTx = await loadFixture(createValidEventPlace);
+
+      await expect(createEventPlaceTx)
         .to.emit(eventManager, "NewEventPlaceAvailable")
         .withArgs(0, ...asArguments(createEventPlaceRequest));
     });
 
     it("should validate min and max tickets", async () => {
-      const { eventManager, master } = await loadFixture(contracts);
+      const { eventManager, master } = await loadFixture(deployContract);
       await expect(
         eventManager
           .connect(master)
@@ -80,15 +124,13 @@ describe("CyberValleyEventManager", () => {
           ),
       ).to.be.revertedWith("Values must be greater than zero");
       await expect(
-        eventManager
-          .connect(master)
-          .createEventPlace(
-            ...asArguments({
-              ...createEventPlaceRequest,
-              minTickets: 0,
-              maxTickets: 0,
-            }),
-          ),
+        eventManager.connect(master).createEventPlace(
+          ...asArguments({
+            ...createEventPlaceRequest,
+            minTickets: 0,
+            maxTickets: 0,
+          }),
+        ),
       ).to.be.revertedWith("Values must be greater than zero");
       await expect(
         eventManager
@@ -100,7 +142,7 @@ describe("CyberValleyEventManager", () => {
     });
 
     it("should validate min price", async () => {
-      const { eventManager, master } = await loadFixture(contracts);
+      const { eventManager, master } = await loadFixture(deployContract);
       await expect(
         eventManager
           .connect(master)
@@ -111,7 +153,7 @@ describe("CyberValleyEventManager", () => {
     });
 
     it("should validate min days", async () => {
-      const { eventManager, master } = await loadFixture(contracts);
+      const { eventManager, master } = await loadFixture(deployContract);
       await expect(
         eventManager
           .connect(master)
@@ -120,5 +162,11 @@ describe("CyberValleyEventManager", () => {
           ),
       ).to.be.revertedWith("Values must be greater than zero");
     });
+
+    it("should revert updating unexisting event place", async () => {});
+
+    it("should update existing event place", async () => {});
+
+    it("should revert updating event for the approved event", async () => {});
   });
 });
