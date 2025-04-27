b2d990df391fecee6804adb45d15fb5b054372d9
scipunch <scipunch@gmail.com>
2025-04-27 23:30:55 +0200

    fix: Migrations

--- backend/cyber_valley/web3_auth/models.py
+++ backend/cyber_valley/web3_auth/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class User(AbstractBaseUser):
-    address = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, primary_key=True)
 
     REQUIRED_FIELDS: ClassVar[list[str]] = []
     USERNAME_FIELD = "address"
