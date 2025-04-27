fd3a72293eaf4a8d09eb6b91ccc617ab9fe00c55
scipunch <scipunch@gmail.com>
2025-04-27 18:15:31 +0200

    add: Apply ruff & mypy

--- backend/cyber_valley/wsgi.py
+++ backend/cyber_valley/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cyber_valley.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cyber_valley.settings")
 
 application = get_wsgi_application()
