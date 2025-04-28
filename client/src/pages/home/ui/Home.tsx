9988f29a980f51d82670643a93237f20db89f146
Danila Mandrabura <danilamandrabura@gmail.com>
2025-04-28 21:10:43 +0300

    Add: home page scratch

--- client/src/pages/home/ui/Home.tsx
+++ client/src/pages/home/ui/Home.tsx
@@ -1,5 +1,7 @@
 import { Nav } from "@/shared/widgets/navigation";
 
+import { Link } from "react-router";
+
 import { BaseEventList } from "@/widgets/BaseEventsList/ui/BaseEventList";
 
 import { events } from "../mock";
@@ -8,8 +10,19 @@ export const Home = () => {
   return (
     <div>
       <Nav />
-      Home
-      <BaseEventList events={events} limit={3} />
+      <div className="w-full h-2 bg-amber-700" />
+      <div className="flex justify-between items-center p-3">
+        <p>You have 6 unread notifications</p>
+        <Link to="/notifications">See all...</Link>
+      </div>
+      <div className="w-full h-2 bg-amber-700" />
+      <section>
+        <div className="flex justify-between items-center p-3">
+          <p>Upcoming events</p>
+          <Link to="/events">See more...</Link>
+        </div>
+        <BaseEventList events={events} limit={3} />
+      </section>
     </div>
   );
 };
