c602a4cf70cd174d21d5b810cf995982b98760a1
scipunch <scipunch@gmail.com>
2025-04-29 20:39:09 +0200

    add: Users current response schema

--- backend/cyber_valley/users/views.py
+++ backend/cyber_valley/users/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
 from .models import CyberValleyUser
 from .serializers import CurrentUserSerializer
@@ -11,6 +12,7 @@ from .serializers import CurrentUserSerializer
 class CurrentUserViewSet(viewsets.GenericViewSet[CyberValleyUser]):
     permission_classes = (IsAuthenticated,)
 
+    @extend_schema(responses=CurrentUserSerializer)
     @action(detail=False, methods=["get"], name="Current user")
     def current(self, request: Request) -> Response:
         serializer = CurrentUserSerializer(request.user)
