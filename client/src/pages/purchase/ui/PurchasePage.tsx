881ebc1ea392d00779de62e918e54ca3b8925bab
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-01 10:55:37 +0300

    Add: flow from puy ticket to checkout

--- client/src/pages/purchase/ui/PurchasePage.tsx
+++ client/src/pages/purchase/ui/PurchasePage.tsx
@@ -1,3 +1,14 @@
+import { useOrderStore } from "@/entities/order";
+import { Navigate } from "react-router";
+
 export const PurchasePage: React.FC = () => {
-  return <div>Purchase</div>;
+  const { order } = useOrderStore();
+
+  if (!order) return <Navigate to="/" />;
+
+  return (
+    <div>
+      <h2 className="text-5xl text-center py-30">{order.type}</h2>
+    </div>
+  );
 };
