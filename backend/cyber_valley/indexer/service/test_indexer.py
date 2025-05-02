024c88efaa934a3ecd24ceccd6cf722171d4c92a
scipunch <scipunch@gmail.com>
2025-05-02 21:33:06 +0200

    add: Fetching all logs for the given addresses

--- backend/cyber_valley/indexer/service/test_indexer.py
+++ backend/cyber_valley/indexer/service/test_indexer.py
@@ -1,3 +1,4 @@
+import json
 import subprocess
 import threading
 from collections.abc import Callable, Generator
@@ -12,6 +13,7 @@ from hexbytes import HexBytes
 from pytest_print import Printer
 from web3 import Web3
 from web3.contract import Contract
+from web3.types import LogReceipt
 from web3.utils.address import get_create_address
 
 ETHEREUM_DIR: Final = Path(__file__).parent.parent.parent.parent.parent / "ethereum"
@@ -104,6 +106,12 @@ def _execute(
     print("End of", command)
 
 
+def _wait_for_line(proc: subprocess.Popen[str], return_after_line: str) -> None:
+    assert proc.stdout
+    while return_after_line not in proc.stdout.readline():
+        pass
+
+
 def _get_all_contracts(
     w3: Web3, *, from_block: int = 0, to_block: int | Literal["latest"] = "latest"
 ) -> dict[ChecksumAddress, Contract]:
@@ -121,19 +129,22 @@ def _get_all_contracts(
         )
     assert len(contract_addresses) == len(CONTRACTS_INFO)
     return {
-        address: w3.eth.contract(address=address, abi=CONTRACTS_INFO[idx].read_text())
+        address: w3.eth.contract(
+            address=address, abi=json.loads(CONTRACTS_INFO[idx].read_text())["abi"]
+        )
         for idx, address in enumerate(contract_addresses)
     }
 
 
-def _wait_for_line(proc: subprocess.Popen[str], return_after_line: str) -> None:
-    assert proc.stdout
-    while return_after_line not in proc.stdout.readline():
-        pass
+def _get_logs(w3: Web3, contracts: list[ChecksumAddress]) -> list[LogReceipt]:
+    return w3.eth.filter(
+        {"fromBlock": 0, "toBlock": "latest", "address": contracts}
+    ).get_all_entries()
 
 
 def test_create_event(w3: Web3, run_hardhat_test: HardhatTestRunner) -> None:
     with run_hardhat_test("createEvent"):
         contracts = _get_all_contracts(w3)
-        print(f"{contracts.keys()=}")
+        logs = _get_logs(w3, list(contracts.keys()))
+        print(f"Got {len(logs)} logs")
         pytest.fail("Not implemented")
