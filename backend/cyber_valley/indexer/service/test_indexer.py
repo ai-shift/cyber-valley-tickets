6c26e3524b9090be050a63b9882e1432258ba738
scipunch <scipunch@gmail.com>
2025-05-02 20:17:21 +0200

    add: Create contracts from addresses

--- backend/cyber_valley/indexer/service/test_indexer.py
+++ backend/cyber_valley/indexer/service/test_indexer.py
@@ -7,12 +7,35 @@ from pathlib import Path
 from typing import Final, Literal
 
 import pytest
+from eth_typing import ChecksumAddress
+from hexbytes import HexBytes
 from pytest_print import Printer
 from web3 import Web3
+from web3.contract import Contract
 from web3.utils.address import get_create_address
 
 ETHEREUM_DIR: Final = Path(__file__).parent.parent.parent.parent.parent / "ethereum"
 ETH_NETWORK_HOST: Final = "localhost:8545"
+CONTRACTS_INFO: Final = (
+    (
+        ETHEREUM_DIR
+        / "artifacts/contracts/mocks/SimpleERC20Xylose.sol"
+        / "SimpleERC20Xylose.json"
+    ),
+    (
+        ETHEREUM_DIR
+        / "artifacts/contracts/CyberValleyEventManager.sol"
+        / "CyberValleyEventManager.json"
+    ),
+    (
+        ETHEREUM_DIR
+        / "artifacts/contracts/CyberValleyEventTicket.sol/"
+        / "CyberValleyEventTicket.json"
+    ),
+)
+"""
+Contains contracts and their ABI in order of deployment
+"""
 
 ProcessStarter = Generator[None]
 
@@ -83,19 +106,24 @@ def _execute(
 
 def _get_all_contracts(
     w3: Web3, *, from_block: int = 0, to_block: int | Literal["latest"] = "latest"
-) -> list[str]:
-    contract_addresses = []
+) -> dict[ChecksumAddress, Contract]:
+    contract_addresses: list[ChecksumAddress] = []
     for block_number in range(
         from_block, w3.eth.block_number if to_block == "latest" else to_block + 1
     ):
         block = w3.eth.get_block(block_number, full_transactions=True)
+        transactions = block["transactions"]
+
         contract_addresses.extend(
             get_create_address(tx["from"], tx["nonce"])
-            for tx in block["transactions"]
-            if tx["to"] is None
+            for tx in transactions
+            if not isinstance(tx, HexBytes)
         )
-
-    return contract_addresses
+    assert len(contract_addresses) == len(CONTRACTS_INFO)
+    return {
+        address: w3.eth.contract(address=address, abi=CONTRACTS_INFO[idx].read_text())
+        for idx, address in enumerate(contract_addresses)
+    }
 
 
 def _wait_for_line(proc: subprocess.Popen[str], return_after_line: str) -> None:
@@ -107,5 +135,5 @@ def _wait_for_line(proc: subprocess.Popen[str], return_after_line: str) -> None:
 def test_create_event(w3: Web3, run_hardhat_test: HardhatTestRunner) -> None:
     with run_hardhat_test("createEvent"):
         contracts = _get_all_contracts(w3)
-        print(f"{contracts=}")
+        print(f"{contracts.keys()=}")
         pytest.fail("Not implemented")
