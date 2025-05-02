16b0f2d154fa5bbd93424e24e0413f4b7f2700fc
scipunch <scipunch@gmail.com>
2025-05-02 16:32:41 +0200

    add: Post mortem note

--- client/src/entities/event/model/types.ts
+++ client/src/entities/event/model/types.ts
@@ -1,6 +1,7 @@
 import type { components } from "@/shared/api";
 
 export type Event = components["schemas"]["StaffEvent" | "CreatorEvent"];
+// XXX: Post mortem review
 export type EventDto = Pick<
   Event,
   "title" | "description" | "daysAmount" | "ticketPrice"
