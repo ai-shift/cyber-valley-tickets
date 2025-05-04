d64503568f48f5ad84ddfe85585f3d07711e5204
scipunch <scipunch@gmail.com>
2025-05-04 16:04:05 +0200

    add: Update event tests

--- backend/cyber_valley/indexer/service/test_indexer.py
+++ backend/cyber_valley/indexer/service/test_indexer.py
@@ -599,6 +599,74 @@ def test_decline_event(events_factory: EventsFactory) -> None:
     assert len(events) == 0, events
 
 
+def test_update_event(events_factory: EventsFactory) -> None:
+    events = events_factory("updateEvent")
+
+    #  begin-region -- RoleGranted
+    role_to_count = {
+        "MASTER_ROLE": 16,
+        "STAFF_ROLE": 8,
+        "DEFAULT_ADMIN_ROLE": 16,
+        "EVENT_MANAGER_ROLE": 8,
+    }
+    for role, count in role_to_count.items():
+        role_granted_events = [
+            event
+            for event in events
+            if isinstance(event, CyberValleyEventManager.RoleGranted)
+            and event.role == role
+        ]
+        assert len(role_granted_events) == count
+        _cleanup_asserted_events(events, role_granted_events)
+    #  end-region   -- RoleGranted
+
+    #  begin-region -- NewEventPlaceAvailable
+    new_place_available_events = [
+        event
+        for event in events
+        if isinstance(event, CyberValleyEventManager.NewEventPlaceAvailable)
+    ]
+    expected = [
+        {
+            "eventPlaceId": 0,
+            "maxTickets": 100,
+            "minTickets": 50,
+            "minPrice": 20,
+            "minDays": 1,
+        },
+    ]
+    expected_counts = [6]
+    assert len(expected) == len(expected_counts)
+    for event in new_place_available_events:
+        expected_counts[expected.index(event.model_dump(by_alias=True))] -= 1
+    assert sum(expected_counts) == 0
+    _cleanup_asserted_events(events, new_place_available_events)
+    #  end-region   -- NewEventPlaceAvailable
+
+    #  begin-region -- EventPlaceUpdated
+    event_place_updated_events = [
+        event
+        for event in events
+        if isinstance(event, CyberValleyEventManager.EventPlaceUpdated)
+    ]
+    expected = CyberValleyEventManager.EventPlaceUpdated.model_validate(
+        {
+            "eventPlaceId": 0,
+            "maxTickets": 150,
+            "minTickets": 20,
+            "minPrice": 30,
+            "minDays": 2,
+        }
+    )
+    assert all(event == expected for event in event_place_updated_events)
+    _cleanup_asserted_events(events, event_place_updated_events)
+    #  end-region   -- EventPlaceUpdated
+
+    _cleanup_erc_events(events)
+
+    assert len(events) == 0, events
+
+
 def _get_all_contracts(w3: Web3) -> list[type[Contract]]:
     return [
         w3.eth.contract(abi=json.loads(info_path.read_text())["abi"])
@@ -617,9 +685,7 @@ def _cleanup_asserted_events[T: BaseModel](
         events.remove(evt)
 
 
-def _cleanup_erc_events[T: BaseModel](
-    events: list[BaseModel]
-) -> None:
+def _cleanup_erc_events[T: BaseModel](events: list[BaseModel]) -> None:
     _cleanup_asserted_events(
         events,
         [
