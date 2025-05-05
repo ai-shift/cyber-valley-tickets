e267284f801117b78f389561cb1947514e67eb8e
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-05 20:17:10 +0300

    Refactor: app container

--- client/src/shared/widgets/layout/AppContainer.tsx
+++ client/src/shared/widgets/layout/AppContainer.tsx
@@ -9,7 +9,7 @@ export const AppContainer: React.FC<AppContainerProps> = ({ children }) => {
     <div className="w-screen h-screen py-3 flex justify-center items-center">
       <main
         id="appContainer"
-        className="h-full w-full sm:w-96 lg:w-1/3 sm:border-2 rounded-lg border-gray-700 overflow-y-auto overflow-x-hidden"
+        className="h-full w-full sm:w-96 lg:w-1/3 sm:border-2 rounded-3xl border-primary/60 overflow-y-auto overflow-x-hidden p-5"
       >
         {children}
       </main>
