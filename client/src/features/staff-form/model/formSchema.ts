16eebfb47b63897244656b36f401adeaa31dd60a
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-05 09:17:37 +0300

    Add: check for address length

--- client/src/features/staff-form/model/formSchema.ts
+++ client/src/features/staff-form/model/formSchema.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
-// TODO: Check for the address length (42)
 export const formSchema = z.object({
   address: z
     .string()
+    .length(42, "Address should be 42 symbols long")
     .nonempty("Address is required")
     .refine((val) => val.startsWith("0x"), {
       message: "Address must start with '0x'",
