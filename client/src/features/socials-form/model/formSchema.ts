c51f5d909b271eeabb2abe2bf255fff6aba16dbb
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-05 09:16:16 +0300

    Typo

--- client/src/features/socials-form/model/formSchema.ts
+++ client/src/features/socials-form/model/formSchema.ts
@@ -2,7 +2,6 @@ import type { Socials } from "@/entities/order";
 import { type ZodType, z } from "zod";
 
 export const formSchema: ZodType<Socials> = z.object({
-  // TODO: Fix typo
-  type: z.string().min(1, "Select one of the possible metworks"),
+  type: z.string().min(1, "Select one of the possible networks"),
   contactInfo: z.string().min(1, "Enter contact information"),
 });
