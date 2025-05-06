14e28ae5fb5dfff0bf006211f232374aac0f1bd6
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-06 19:05:29 +0300

    Add: auth without login

--- client/src/app/providers/authProvider/api/verify.ts
+++
@@ -1,3 +0,0 @@
-import { apiClient } from "@/shared/api";
-
-export const verify = async () => await apiClient.GET("/api/auth/verify");
--- client/src/app/routes/Router.tsx
+++ client/src/app/routes/Router.tsx
@@ -18,45 +18,53 @@ import {
 } from "@/pages";
 import { NavContainer } from "@/shared/widgets/layout/NavContainer";
 import { QueryProvider } from "../providers";
+import { AuthProvider } from "../providers";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <QueryProvider>
         <Routes>
-          <Route element={<NavContainer />}>
-            <Route path="/" index element={<HomePage />} />
+          <Route element={<AuthProvider />}>
+            <Route element={<NavContainer />}>
+              <Route path="/" index element={<HomePage />} />
 
-            <Route path="/events">
-              <Route index element={<EventsListPage />} />
-              <Route path="/events/:eventId" element={<EventsDetailsPage />} />
-              <Route path="/events/:eventId/edit" element={<EditEventPage />} />
-              <Route path="/events/create" element={<CreateEventPage />} />
+              <Route path="/events">
+                <Route index element={<EventsListPage />} />
+                <Route
+                  path="/events/:eventId"
+                  element={<EventsDetailsPage />}
+                />
+                <Route
+                  path="/events/:eventId/edit"
+                  element={<EditEventPage />}
+                />
+                <Route path="/events/create" element={<CreateEventPage />} />
+              </Route>
+
+              <Route path="/manage">
+                <Route index element={<ManagePage />} />
+                <Route
+                  path="/manage/create-place"
+                  element={<CreatePlacePage />}
+                />
+                <Route
+                  path="/manage/assign-staff"
+                  element={<AssignStaffPage />}
+                />
+              </Route>
+
+              <Route path="/notifications" element={<NotificationsPage />} />
+
+              <Route path="/account">
+                <Route index element={<AccountPage />} />
+                <Route path="/account/my-events" element={<MyEventsPage />} />
+              </Route>
             </Route>
 
-            <Route path="/manage">
-              <Route index element={<ManagePage />} />
-              <Route
-                path="/manage/create-place"
-                element={<CreatePlacePage />}
-              />
-              <Route
-                path="/manage/assign-staff"
-                element={<AssignStaffPage />}
-              />
-            </Route>
-
-            <Route path="/notifications" element={<NotificationsPage />} />
-
-            <Route path="/account">
-              <Route index element={<AccountPage />} />
-              <Route path="/account/my-events" element={<MyEventsPage />} />
-            </Route>
+            <Route path="/purchase" element={<PurchasePage />} />
+            <Route path="/socials" element={<SocialsPage />} />
           </Route>
-
-          <Route path="/purchase" element={<PurchasePage />} />
-          <Route path="/socials" element={<SocialsPage />} />
-
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </QueryProvider>
