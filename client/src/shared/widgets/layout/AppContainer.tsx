c963421821d33efa2ab58aba619b0b0831577403
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-06 10:18:49 +0300

    Fix: containers paddings

--- client/src/shared/widgets/layout/AppContainer.tsx
+++ client/src/shared/widgets/layout/AppContainer.tsx
@@ -9,7 +9,7 @@ export const AppContainer: React.FC<AppContainerProps> = ({ children }) => {
     <div className="w-screen h-screen sm:py-3 flex justify-center items-center">
       <main
         id="appContainer"
-        className="h-full w-full sm:w-2/3 md:w-3/5 lg:w-1/2 xl:w-1/3 sm:border-2 rounded-3xl border-primary/60 p-5"
+        className="h-full w-full sm:w-2/3 md:w-3/5 lg:w-1/2 xl:w-1/3 sm:border-2 rounded-3xl border-primary/60"
       >
         {children}
       </main>
