26d3b69fd2f7bcaf0fb428c65777f3bcc9ff6490
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-06 10:18:49 +0300

    Fix: containers paddings

--- client/src/shared/widgets/layout/NavContainer.tsx
+++ client/src/shared/widgets/layout/NavContainer.tsx
@@ -4,10 +4,10 @@ import { Nav } from "../navigation";
 export const NavContainer: React.FC = () => {
   return (
     <div className="h-full flex flex-col justify-between">
-      <div className="overflow-y-auto flex-1">
+      <div className="overflow-y-auto flex-1 p-3">
         <Outlet />
       </div>
-      <Nav className="bg-background bottom-0 left-0 right-0" />
+      <Nav className="bg-background bottom-0 left-0 right-0 p-2 rounded-b-3xl" />
     </div>
   );
 };
