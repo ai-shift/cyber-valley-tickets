b3a099c1216dd6f43ec627b70f3f851e0cc7a30b
scipunch <scipunch@gmail.com>
2025-05-07 20:50:56 +0200

    fix: Use timestamp for notification create_at field

--- backend/cyber_valley/notifications/serializers.py
+++ backend/cyber_valley/notifications/serializers.py
@@ -5,8 +5,12 @@ from .models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer[Notification]):
     id = serializers.IntegerField(source="notification_id", read_only=True)
+    created_at_timestamp = serializers.SerializerMethodField()
 
     class Meta:
         model = Notification
-        fields = ("id", "title", "body", "seen_at", "created_at")
+        fields = ("id", "title", "body", "seen_at", "created_at_timestamp")
         read_only_fields = fields
+
+    def get_created_at_timestamp(self, obj: Notification) -> int:
+        return int(obj.created_at.timestamp())
