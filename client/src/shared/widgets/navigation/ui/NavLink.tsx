f66378153fffcfeef5f49c66cc965aba7d6a654f
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-07 20:42:35 +0300

    Refactor: add icons to nav

--- client/src/shared/widgets/navigation/ui/NavLink.tsx
+++ client/src/shared/widgets/navigation/ui/NavLink.tsx
@@ -1,6 +1,6 @@
 import type { Role } from "@/shared/lib/RBAC";
 import type { Route } from "../model/routes";
-
+import { useLocation } from "react-router";
 import { NavLink as Link } from "react-router";
 
 type NavLinkProps = {
@@ -10,6 +10,10 @@ type NavLinkProps = {
 
 export const NavLink: React.FC<NavLinkProps> = ({ route, role }) => {
   const { path, title, restrictedTo } = route;
+  const { pathname } = useLocation();
+
+  const isCurrent = pathname === path;
+  const icon = title.toLowerCase();
 
   const canDisplay = !restrictedTo || restrictedTo.includes(role);
 
@@ -18,9 +22,14 @@ export const NavLink: React.FC<NavLinkProps> = ({ route, role }) => {
       <Link
         to={path}
         className={({ isActive }) =>
-          `flex-1 text-center clip-corners p-4 border-[1px] border-primary font-semibold ${isActive ? "text-black bg-primary" : "text-white"}`
+          `flex-1 flex flex-col items-center text-center clip-corners px-4 py-2 border-[1px] border-primary font-semibold ${isActive ? "text-black bg-primary" : "text-primary"}`
         }
       >
+        <img
+          className="aspect-square h-8"
+          src={`/icons/${isCurrent ? `${icon}_active` : icon}.svg`}
+          alt={title}
+        />
         {title}
       </Link>
     )
