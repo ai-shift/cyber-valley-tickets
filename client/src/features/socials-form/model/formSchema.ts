9790225b032edb3aa004a997f9bbfba07c0d0dfd
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-07 19:53:43 +0300

    Refactor: socials update types

--- client/src/features/socials-form/model/formSchema.ts
+++ client/src/features/socials-form/model/formSchema.ts
@@ -2,6 +2,6 @@ import type { Socials } from "@/entities/order";
 import { type ZodType, z } from "zod";
 
 export const formSchema: ZodType<Socials> = z.object({
-  type: z.enum(["telegram", "instagram", "discord", "whatsapp"]),
+  type: z.string().min(1, "Pick one of social networks"),
   contactInfo: z.string().min(1, "Enter contact information"),
 });
