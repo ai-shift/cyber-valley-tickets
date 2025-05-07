e7f6d41259610e0541dc7d95a31ba6d71c3ee2c3
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-07 20:44:42 +0300

    Add: purchase mutation, add mutation

--- client/src/features/purchase/ui/ConfirmPayment.tsx
+++ client/src/features/purchase/ui/ConfirmPayment.tsx
@@ -1,30 +1,62 @@
 import type { Order } from "@/entities/order";
 import { Button } from "@/shared/ui/button";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 import { purchase } from "../api/purchase";
+import { SuccessDialog } from "./SuccessDialog";
 
 type ConfirmPaymentProps = {
   order: Order;
 };
 export const ConfirmPayment: React.FC<ConfirmPaymentProps> = ({ order }) => {
-  const { mutate } = useMutation({
+  const navigate = useNavigate();
+  const [success, setSuccess] = useState(false);
+  const { mutate, error } = useMutation({
     mutationFn: purchase,
+    onSuccess: () => {
+      setSuccess(true);
+    },
   });
+
+  const navigateFn = () => navigate("/", { replace: true });
+  const successMessage =
+    order.type === "buy_ticket"
+      ? "Your will recieve your ticket within 15 minutes."
+      : "Your order will be published within 15 minutes.";
 
   return (
     <article className="card border-primary/30">
-      <img
-        className="aspect-square h-40 mx-auto my-7"
-        src="/icons/price_2.svg"
-        alt="purchase"
-      />
-      <h2 className="text-muted font-semibold text-lg text-center">
-        Connect Your Wallet
-      </h2>
-      <p className="text-muted/70 text-md text-center">
-        Connect your Web3 wallet to complete the purchase. You'll be prompted to
-        confirm the transaction.
-      </p>
+      {error ? (
+        <div>
+          <img
+            className="aspect-square h-40 mx-auto my-7"
+            src="/icons/price_2.svg"
+            alt="purchase"
+          />
+          <h2 className="text-muted font-semibold text-lg text-center">
+            Errors during transaction
+          </h2>
+          <p className="text-muted/70 text-md text-center">
+            Check your wallet for the details or try again later.
+          </p>
+        </div>
+      ) : (
+        <div>
+          <img
+            className="aspect-square h-40 mx-auto my-7"
+            src="/icons/price_2.svg"
+            alt="purchase"
+          />
+          <h2 className="text-muted font-semibold text-lg text-center">
+            Connect Your Wallet
+          </h2>
+          <p className="text-muted/70 text-md text-center">
+            Connect your Web3 wallet to complete the purchase. You'll be
+            prompted to confirm the transaction.
+          </p>
+        </div>
+      )}
       <div className="flex justify-center py-6">
         <span>
           <Button onClick={() => mutate(order)} className="mx-auto">
@@ -32,6 +64,12 @@ export const ConfirmPayment: React.FC<ConfirmPaymentProps> = ({ order }) => {
           </Button>
         </span>
       </div>
+      <SuccessDialog
+        open={success}
+        setOpen={setSuccess}
+        successMsg={successMessage}
+        navigateFn={navigateFn}
+      />
     </article>
   );
 };
