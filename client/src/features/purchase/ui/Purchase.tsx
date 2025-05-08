47e827bae0ab1cfd7d2584ed6ecedf0f8b64603a
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-09 00:41:06 +0300

    Add: Conditional rendering in purchase

--- client/src/features/purchase/ui/Purchase.tsx
+++ client/src/features/purchase/ui/Purchase.tsx
@@ -9,7 +9,12 @@ export const Purchase: React.FC = () => {
   return (
     <div className="flex flex-col py-5 px-4 gap-5">
       {order?.type === "buy_ticket" && <PurchaseTicket ticket={order.ticket} />}
-      {order?.type === "create_event" && <PurchaseEvent event={order.event} />}
+      {order?.type === "create_event" && (
+        <PurchaseEvent type={order.type} event={order.event} />
+      )}
+      {order?.type === "update_event" && (
+        <PurchaseEvent type={order.type} event={order.event} />
+      )}
 
       {order && <ConfirmPayment order={order} />}
     </div>
