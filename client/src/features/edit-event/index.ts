3dad29be302fd30a1a6ba01f17f645e96a742c9d
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-09 00:42:52 +0300

    Add: edit event as a feature

---
+++ client/src/features/edit-event/index.ts
@@ -0,0 +1,1 @@
+export { EditEventAction } from "./ui/EditEventAction";
---
+++ client/src/features/edit-event/ui/EditEventAction.tsx
@@ -0,0 +1,34 @@
+import type { EventDto } from "@/entities/event";
+import { useOrderStore } from "@/entities/order";
+import { useUser } from "@/entities/user";
+import { canUserEdit, EditEvent } from "@/features/create-edit-event";
+import { useNavigate } from "react-router";
+
+type EditEventActionProps = {
+  numbericId: number;
+};
+
+export const EditEventAction: React.FC<EditEventActionProps> = ({
+  numbericId,
+}) => {
+  const navigate = useNavigate();
+  const { user } = useUser();
+  const { setEventOrder } = useOrderStore();
+
+  if (!user) return <p>I hate this</p>;
+
+  function onSubmit(values: EventDto) {
+    setEventOrder({ ...values, id: numbericId });
+    navigate("/socials");
+  }
+
+  return (
+    <div>
+      <EditEvent
+        editEventId={numbericId}
+        onSubmit={onSubmit}
+        canEdit={canUserEdit(user)}
+      />
+    </div>
+  );
+};
