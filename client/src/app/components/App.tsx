f8d901e9218023aa8f3e5b1c5a61cece2452c900
scipunch <scipunch@gmail.com>
2025-05-08 03:58:56 +0200

    add: Install thirdweb

--- client/src/app/components/App.tsx
+++ client/src/app/components/App.tsx
@@ -1,10 +1,13 @@
 import { AppContainer } from "@/shared/widgets/layout/AppContainer";
+import { ThirdwebProvider } from "thirdweb/react";
 import { Router } from "../routes/Router";
 
 export const App = () => {
   return (
     <AppContainer>
-      <Router />
+      <ThirdwebProvider>
+        <Router />
+      </ThirdwebProvider>
     </AppContainer>
   );
 };
