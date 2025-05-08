b852a72bdf6c1bee1e9b88e8a88dd78db880dba7
scipunch <scipunch@gmail.com>
2025-05-08 06:03:43 +0200

    add: Damn contracts work again

--- client/src/app/providers/authProvider/ui/AuthProvider.tsx
+++ client/src/app/providers/authProvider/ui/AuthProvider.tsx
@@ -1,9 +1,11 @@
 import type React from "react";
 import { refresh } from "../api/refresh";
 
+import { client, cvlandChain, wallets } from "@/shared/lib/web3";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router";
+import { useAutoConnect } from "thirdweb/react";
 import { useRefreshSlice } from "../model/refreshSlice";
 
 export const AuthProvider: React.FC = () => {
@@ -20,6 +22,12 @@ export const AuthProvider: React.FC = () => {
 
   const hasError = !isLoading && isError;
   const { setHasJWT, hasJWT } = useRefreshSlice();
+  const { accountIsLoading } = useAutoConnect({
+    client,
+    wallets,
+    onConnect: (connecting) => console.log("onConnect", connecting),
+    timeout: 5000,
+  });
 
   useEffect(() => {
     if (!hasJWT) {
@@ -31,5 +39,6 @@ export const AuthProvider: React.FC = () => {
       navigate("/login");
     }
   }, [hasError, navigate, hasJWT, setHasJWT]);
+
   return <Outlet />;
 };
--- client/src/features/create-place/ui/CreatePlace.tsx
+++ client/src/features/create-place/ui/CreatePlace.tsx
@@ -1,15 +1,18 @@
 import { type EventPlaceForm, PlaceForm } from "@/features/place-form";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import { useActiveAccount, useActiveWallet } from "thirdweb/react";
 import { create } from "../api/create";
 import { PlaceSuccessDialog } from "./PlaceSuccessDialog";
 
 export const CreatePlace: React.FC = () => {
   const [isSuccess, setIsSuccess] = useState(false);
+  const account = useActiveAccount();
 
   const { mutate } = useMutation({
-    mutationFn: (values: EventPlaceForm) => create(values),
-    onSuccess: () => {},
+    mutationFn: (values: EventPlaceForm) => create(values, account),
+    onSuccess: console.log,
+    onError: console.error,
   });
 
   return (
