660518e640dd6ea9c1799e83ab950db4215ecbd5
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-09 00:37:50 +0300

    Refactor + add: add event and refactor it with socials

--- client/src/features/purchase/api/purchase.tsx
+++ client/src/features/purchase/api/purchase.tsx
@@ -1,8 +1,13 @@
-import { mintTicket, submitEventRequest } from "@/shared/lib/web3";
+import {
+  mintTicket,
+  submitEventRequest,
+  updateEvent as updateEventContract,
+} from "@/shared/lib/web3";
 import type { Account } from "thirdweb/wallets";
 
 import type { Order } from "@/entities/order";
 import { apiClient } from "@/shared/api";
+import type { EventDto } from "@/entities/event";
 
 export const purchase = async (account: Account, order: Order) => {
   const pickFetch: {
@@ -10,6 +15,7 @@ export const purchase = async (account: Account, order: Order) => {
   } = {
     create_event: purchaseEvent,
     buy_ticket: purchaseTicket,
+    update_event: updateEvent,
   };
 
   await pickFetch[order.type](account, order);
@@ -18,19 +24,38 @@ export const purchase = async (account: Account, order: Order) => {
 const purchaseTicket = async (account: Account, order: Order) => {
   if (order.type !== "buy_ticket")
     throw new Error("There is no ticket in the order");
-  if (!order.socials) throw new Error("There is no socials in the order");
 
-  const { data } = await apiClient.PUT("/api/ipfs/users/socials", {
-    body: {
-      //@ts-ignore
-      network: order.socials.type.toLocaleLowerCase(),
-      value: order.socials.contactInfo,
-    },
-  });
+  const { data } = await getSocialsCid(order);
 
   if (!data || !data.cid) throw new Error("Can't fetch CID");
 
   await mintTicket(account, order.ticket.eventId, data.cid);
+};
+
+const updateEvent = async (account: Account, order: Order) => {
+  if (order.type !== "update_event")
+    throw new Error("There is no event in the order");
+
+  const { data: socialsData } = await getSocialsCid(order);
+  if (!socialsData || !socialsData.cid) throw new Error("Can't fetch CID");
+
+  const { data: eventData } = await getEventCid(order.event, socialsData.cid);
+
+  if (!eventData || !eventData.cid)
+    throw new Error("Can't fetch event meta CID");
+
+  const { id, place, ticketPrice, startTimeTimeStamp, daysAmount } =
+    order.event;
+
+  await updateEventContract(
+    account,
+    id,
+    place,
+    ticketPrice,
+    startTimeTimeStamp,
+    daysAmount,
+    eventData.cid,
+  );
 };
 
 const purchaseEvent = async (account: Account, order: Order) => {
@@ -38,40 +63,13 @@ const purchaseEvent = async (account: Account, order: Order) => {
     throw new Error("There is no event in the order");
   if (!order.socials) throw new Error("There is no socials in the order");
 
-  const {
-    place,
-    ticketPrice,
-    startTimeTimeStamp,
-    daysAmount,
-    title,
-    description,
-    image,
-  } = order.event;
+  const { place, ticketPrice, startTimeTimeStamp, daysAmount } = order.event;
 
-  console.log(order.socials);
-
-  const { data: socialsData } = await apiClient.PUT("/api/ipfs/users/socials", {
-    body: {
-      //@ts-ignore
-      network: order.socials.type.toLocaleLowerCase(),
-      value: order.socials.contactInfo,
-    },
-  });
-
+  const { data: socialsData } = await getSocialsCid(order);
   if (!socialsData || !socialsData.cid) throw new Error("Can't fetch CID");
 
-  const formData = new FormData();
-  formData.set("title", title);
-  formData.set("description", description);
-  formData.set("cover", image);
-  formData.set("socials_cid", socialsData.cid);
+  const { data: eventData } = await getEventCid(order.event, socialsData.cid);
 
-  const { data: eventData } = await apiClient.PUT("/api/ipfs/events/meta", {
-    //@ts-ignore
-    body: formData,
-  });
-
-  console.log(eventData);
   if (!eventData || !eventData.cid)
     throw new Error("Can't fetch event meta CID");
 
@@ -83,4 +81,30 @@ const purchaseEvent = async (account: Account, order: Order) => {
     daysAmount,
     eventData.cid,
   );
+};
+
+const getSocialsCid = async (order: Order) => {
+  if (!order.socials) throw new Error("There is no socials in the order");
+  return await apiClient.PUT("/api/ipfs/users/socials", {
+    body: {
+      //@ts-ignore
+      network: order.socials.type.toLocaleLowerCase(),
+      value: order.socials.contactInfo,
+    },
+  });
+};
+
+const getEventCid = async (event: EventDto, cid: string) => {
+  const { title, description, image } = event;
+
+  const formData = new FormData();
+  formData.set("title", title);
+  formData.set("description", description);
+  formData.set("cover", image);
+  formData.set("socials_cid", cid);
+
+  return await apiClient.PUT("/api/ipfs/events/meta", {
+    //@ts-ignore
+    body: formData,
+  });
 };
