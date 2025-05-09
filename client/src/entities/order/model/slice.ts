ee4ad5002ede9ea465e99d4dc6bec063d236f608
scipunch <scipunch@gmail.com>
2025-05-09 21:11:18 +0200

    fix: Duplicate import

--- client/src/entities/order/model/slice.ts
+++ client/src/entities/order/model/slice.ts
@@ -1,9 +1,7 @@
 import type { EventDto, EventDtoWithId } from "@/entities/event/@x/order";
-import type { Order, OrderTicket, Socials } from "./types";
-
-import type { EventDtoWithId } from "@/entities/event/model/types";
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
+import type { Order, OrderTicket, Socials } from "./types";
 
 interface OrderState {
   order: Order | null;
