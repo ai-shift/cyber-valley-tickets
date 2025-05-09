6b55b40dca1c9ab6ba2cff163ade362d90f64298
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-09 13:43:35 +0300

    Refactor: create plase update modal logic, rename function

--- client/src/features/create-place/ui/CreatePlace.tsx
+++ client/src/features/create-place/ui/CreatePlace.tsx
@@ -2,23 +2,35 @@ import { type EventPlaceForm, PlaceForm } from "@/features/place-form";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import { useActiveAccount } from "thirdweb/react";
-import { create } from "../api/create";
-import { PlaceSuccessDialog } from "./PlaceSuccessDialog";
+import { createPlaceW3 } from "../api/createPlaceW3";
+import { type ModalStatus, PlaceDialog } from "./PlaceDialog";
 
 export const CreatePlace: React.FC = () => {
-  const [isSuccess, setIsSuccess] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [status, setStatus] = useState<ModalStatus>("");
   const account = useActiveAccount();
 
   const { mutate, isPending } = useMutation({
-    mutationFn: (values: EventPlaceForm) => create(values, account),
-    onSuccess: console.log,
-    onError: console.error,
+    mutationFn: (values: EventPlaceForm) => createPlaceW3(values, account),
+    onSuccess: () => {
+      setStatus("success");
+      setIsOpen(true);
+    },
+    onError: () => {
+      setStatus("error");
+      setIsOpen(true);
+    },
   });
 
   return (
     <div className="px-6">
       <PlaceForm disableFields={isPending} onSubmit={mutate} />
-      <PlaceSuccessDialog open={isSuccess} setOpen={setIsSuccess} />
+      <PlaceDialog
+        open={isOpen}
+        setOpen={setIsOpen}
+        status={status}
+        clearStatus={() => setStatus("")}
+      />
     </div>
   );
 };
--- client/src/shared/ui/CustomModal.tsx
+++ client/src/shared/ui/CustomModal.tsx
@@ -57,7 +57,7 @@ export const CustomModalWindow: React.FC<WindowProps> = ({
   if (!open) return null;
 
   return createPortal(
-    <div className="fixed top-0 left-0 w-full h-screen bg-black/30 z-40 transition-all duration-500">
+    <div className="fixed top-0 left-0 w-full h-screen bg-black/50 z-40 transition-all duration-500">
       <div
         className={cn(
           "fixed top-[50%] left-[50%] -translate-y-[50%] -translate-x-[50%] bg-background border-white py-16 px-8 border-[1px] w-[95%]  sm:max-w-1/2 lg:max-w-1/3 transition-all duration-500",
