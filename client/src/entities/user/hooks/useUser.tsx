114091c201e151e2a49fa78f6f74f25ddc3176f3
scipunch <scipunch@gmail.com>
2025-05-12 17:14:07 +0200

    add: Todos

--- client/src/entities/user/hooks/useUser.tsx
+++ client/src/entities/user/hooks/useUser.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { userQueries } from "../api/userQueries";
 
+// TODO: Move into single file
 export const useUser = () => {
   const { data: user, isLoading } = useQuery(userQueries.current());
   return { user, isLoading };
