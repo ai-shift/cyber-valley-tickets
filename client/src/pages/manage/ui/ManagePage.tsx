a89888faa6e828fc79cd2ee60798d6eaca0e7ed7
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-14 20:19:11 +0300

    Fix: remove back icons from pages

--- client/src/pages/manage/ui/ManagePage.tsx
+++ client/src/pages/manage/ui/ManagePage.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 
 export const ManagePage: React.FC = () => {
   return (
-    <PageContainer name="Manage">
+    <PageContainer hasBackIcon={false} name="Manage">
       <div className="flex flex-col gap-7 px-5 py-9">
         <Link
           className="card border-primary/30 text-center text-xl py-5"
--- client/src/shared/ui/PageContainer.tsx
+++ client/src/shared/ui/PageContainer.tsx
@@ -6,11 +6,13 @@ import { useNavigate } from "react-router";
 type PageContainerProps = {
   name: string;
   children: ReactNode;
+  hasBackIcon?: boolean;
 };
 
 export const PageContainer: React.FC<PageContainerProps> = ({
   children,
   name,
+  hasBackIcon = true,
 }) => {
   const navigate = useNavigate();
   function goBack() {
@@ -19,10 +21,12 @@ export const PageContainer: React.FC<PageContainerProps> = ({
 
   return (
     <div className="w-full flex flex-col items-stretch justify-start">
-      <header className="flex justify-start items-center gap-5 py-4 px-2">
-        <button className="cursor-pointer" type="button" onClick={goBack}>
-          <ArrowLeft size={30} />
-        </button>
+      <header className="flex justify-start items-center gap-5 py-4 px-5">
+        {hasBackIcon && (
+          <button className="cursor-pointer" type="button" onClick={goBack}>
+            <ArrowLeft size={30} />
+          </button>
+        )}
         <h2 className="text-2xl font-semibold text-primary text-shadow-primary text-shadow-xs">
           {name}
         </h2>
