143ebba49ed101a8a80f660954b476ebf4c55738
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-16 18:56:50 +0300

    Add: Place-list feature

---
+++ client/src/features/places-list/ui/PlacesList.tsx
@@ -0,0 +1,29 @@
+import { placesQueries } from "@/entities/place";
+import { NavigateUpdatePlace } from "@/features/create-edit-place/ui/NavigateUpdatePlace";
+import { ErrorMessage } from "@/shared/ui/ErrorMessage";
+import { Loader } from "@/shared/ui/Loader";
+import { ManageItem } from "@/widgets/ManageItem";
+import { useQuery } from "@tanstack/react-query";
+
+export const PlacesList: React.FC = () => {
+  const { data: places, isLoading, error } = useQuery(placesQueries.list());
+
+  if (isLoading) return <Loader />;
+  if (error) return <ErrorMessage errors={error} />;
+  if (!places)
+    return (
+      <ErrorMessage errors={new Error("Some problem with getting places")} />
+    );
+
+  return (
+    <ul className="flex flex-col gap-3 py-5">
+      {places.map((place) => (
+        <ManageItem
+          key={place.id}
+          title={place.title}
+          render={() => [<NavigateUpdatePlace key={place.id} place={place} />]}
+        />
+      ))}
+    </ul>
+  );
+};
