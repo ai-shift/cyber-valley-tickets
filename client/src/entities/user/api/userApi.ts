0c3a1dcf9f4fd5d334811dbdeed61c6a98e68b9f
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-16 15:54:35 +0300

    Add: get staff api + update files structure

--- client/src/entities/user/api/userApi.ts
+++ client/src/entities/user/api/userApi.ts
@@ -2,3 +2,6 @@ import { apiClient } from "@/shared/api";
 
 export const getCurrentUser = async () =>
   await apiClient.GET("/api/users/current/");
+
+export const getUsersStaff = async () =>
+  await apiClient.GET("/api/users/staff/");
