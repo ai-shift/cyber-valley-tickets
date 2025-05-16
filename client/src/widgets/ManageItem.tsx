18467e7f3a1296dd3c578e4cfc878367358a27cf
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-16 18:47:36 +0300

    Add: widget for displaying manage item

---
+++ client/src/widgets/ManageItem.tsx
@@ -0,0 +1,20 @@
+type ManageItemProps = {
+  title: string;
+  render: () => React.ReactNode[];
+};
+
+export const ManageItem: React.FC<ManageItemProps> = ({ title, render }) => {
+  return (
+    <li className="card border-primary/30 flex justify-between items-center">
+      <h3 className=" text-lg">{title}</h3>
+      <ul className="flex flex-row gap-3">
+        {render().map((node, index) => (
+          // biome-ignore lint/suspicious/noArrayIndexKey: The order won't ever change
+          <li className="h-12" key={index}>
+            {node}
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+};
