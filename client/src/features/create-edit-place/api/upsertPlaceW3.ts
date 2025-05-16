006eb5e1140a919927157d747e0cc8fc2dd1938c
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-16 22:45:15 +0300

    Refactor: enhance place w3 call with edit func

---
+++ client/src/features/create-edit-place/api/upsertPlaceW3.ts
@@ -0,0 +1,57 @@
+import type { EventPlaceForm } from "@/features/place-form";
+import { apiClient } from "@/shared/api";
+import { createPlace, updatePlace } from "@/shared/lib/web3";
+import type { Account } from "thirdweb/wallets";
+
+export const upsertPlaceW3 = async (
+  place: EventPlaceForm,
+  account?: Account,
+  editPlaceId?: number,
+) => {
+  if (!account) throw "Account should be defined";
+  const {
+    maxTickets,
+    minPrice,
+    minTickets,
+    minDays,
+    daysBeforeCancel,
+    title,
+    available,
+  } = place;
+
+  const placeForm = new FormData();
+  placeForm.append("title", title);
+  placeForm.append("description", "foo");
+
+  const { data } = await apiClient.PUT("/api/ipfs/places/meta", {
+    // @ts-ignore
+    body: placeForm,
+  });
+
+  if (!data || !data.cid) throw new Error("No cid was recieved");
+
+  if (editPlaceId) {
+    await updatePlace(
+      account,
+      editPlaceId,
+      maxTickets,
+      minTickets,
+      minPrice,
+      daysBeforeCancel,
+      minDays,
+      available,
+      data.cid,
+    );
+  } else {
+    await createPlace(
+      account,
+      maxTickets,
+      minTickets,
+      minPrice,
+      daysBeforeCancel,
+      minDays,
+      available,
+      data.cid,
+    );
+  }
+};
