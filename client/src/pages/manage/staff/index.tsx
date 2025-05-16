9066cfc050f1adb82e51767059c99503c1f400df
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-16 18:50:13 +0300

    Add: staff page with form and list

--- client/src/pages/manage/staff/index.tsx
+++ client/src/pages/manage/staff/index.tsx
@@ -1,5 +1,12 @@
+import { StaffForm } from "@/features/staff-form";
+import { StaffList } from "@/features/staff-list";
 import { PageContainer } from "@/shared/ui/PageContainer";
 
 export const ManageStaffPage: React.FC = () => {
-  return <PageContainer name="Manage staff">Staff</PageContainer>;
+  return (
+    <PageContainer name="Manage staff">
+      <StaffForm />
+      <StaffList />
+    </PageContainer>
+  );
 };
