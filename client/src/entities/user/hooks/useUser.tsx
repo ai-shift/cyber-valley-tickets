67d91ea0fdfa3a1aca51e8f5accf6341e3ac6561
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-16 15:53:51 +0300

    Refactor: move useUser hook

--- client/src/entities/user/hooks/useUser.tsx
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { userQueries } from "../api/userQueries";
-
-// TODO: Move into single file
-export const useUser = () => {
-  const { data: user, isLoading } = useQuery(userQueries.current());
-  return { user, isLoading };
-};
