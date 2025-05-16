e8cf718a7a593f21bc5bb1b6afcf74cc1fc7a49d
Danila Mandrabura <danilamandrabura@gmail.com>
2025-05-16 16:38:23 +0300

    Refactor: pages slice refactor

---
+++ client/src/pages/manage/staff/index.tsx
@@ -0,0 +1,5 @@
+import { PageContainer } from "@/shared/ui/PageContainer";
+
+export const ManageStaffPage: React.FC = () => {
+  return <PageContainer name="Manage staff">Staff</PageContainer>;
+};
--- client/src/pages/socials/index.ts
+++
@@ -1,1 +0,0 @@
-export { SocialsPage } from "./ui/SocialsPage";
