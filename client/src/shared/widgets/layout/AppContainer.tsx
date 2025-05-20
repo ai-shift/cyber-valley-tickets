916ab58839e1e501907afa8df3b96e080c490c73
scipunch <scipunch@gmail.com>
2025-05-20 18:22:31 +0200

    feat: Try to set proper height in safari

--- client/src/shared/widgets/layout/AppContainer.tsx
+++ client/src/shared/widgets/layout/AppContainer.tsx
@@ -6,7 +6,7 @@ type AppContainerProps = {
 
 export const AppContainer: React.FC<AppContainerProps> = ({ children }) => {
   return (
-    <div className="h-svh sm:py-3 flex justify-center items-center">
+    <div className="min-safe-h-screen sm:py-3 flex justify-center items-center">
       <main
         id="appContainer"
         className="h-full w-full md:w-8/12 lg:w-1/2 xl:w-5/12 2xl:w-4/12 md:border-2 rounded-3xl border-primary/60 md:overflow-hidden"
