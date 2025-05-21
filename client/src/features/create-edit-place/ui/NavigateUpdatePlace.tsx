86a6f03dde4610cb750f26ab56a7a3dfdad26f6a
scipunch <scipunch@gmail.com>
2025-05-21 18:37:21 +0200

    style: Fix formatting in NavigateUpdatePlace component

--- client/src/features/create-edit-place/ui/NavigateUpdatePlace.tsx
+++ client/src/features/create-edit-place/ui/NavigateUpdatePlace.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router";
 type NavigateUpdatePlaceProps = {
   place: EventPlace;
 };
+
 export const NavigateUpdatePlace: React.FC<NavigateUpdatePlaceProps> = ({
   place,
 }) => {
