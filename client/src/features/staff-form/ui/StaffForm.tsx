02bbd1bdc8183878e67734aec4dd611c0ee8a32d
scipunch <scipunch@gmail.com>
2025-05-21 18:44:08 +0200

    fixup! style: Improve staff list styling and spacing

--- client/src/features/staff-form/ui/StaffForm.tsx
+++ client/src/features/staff-form/ui/StaffForm.tsx
@@ -33,7 +33,7 @@ export const StaffForm: React.FC = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5 p-5">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
         <FormField
           control={form.control}
           name="address"
