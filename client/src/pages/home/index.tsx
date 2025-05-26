64988bdaad79872b86188bc562df0a566146f28a
scipunch <scipunch@gmail.com>
2025-05-26 21:40:41 +0200

    feat: Add ERC20 mint button to account page

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -2,14 +2,10 @@ import { useNavigate } from "react-router";
 
 import { EventsList, upcomingFilter } from "@/features/events-list/";
 import { NotificationIcon } from "@/features/notifications";
-import { mintERC20 } from "@/shared/lib/web3";
 import { Button } from "@/shared/ui/button";
-import { useActiveAccount } from "thirdweb/react";
 
 export const HomePage: React.FC = () => {
-  const account = useActiveAccount();
   const navigate = useNavigate();
-  if (!account) return <p>Connect your wallet first</p>;
   return (
     <div className="px-6">
       <h1 className="text-shadow-md text-shadow-primary text-primary text-5xl py-4">
@@ -30,16 +26,6 @@ export const HomePage: React.FC = () => {
       <section className="mt-8">
         <EventsList filterFn={upcomingFilter} />
       </section>
-      <Button
-        className="mt-8"
-        onClick={() =>
-          mintERC20(account, 50n)
-            .then(() => alert("Minted 50 tokens"))
-            .catch(console.error)
-        }
-      >
-        Mint ERC20
-      </Button>
     </div>
   );
 };
