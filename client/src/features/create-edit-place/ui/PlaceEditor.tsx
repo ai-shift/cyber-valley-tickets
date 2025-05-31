cc89aef528af0469f7b542ca199b479f1a81c2f2
scipunch <scipunch@gmail.com>
2025-05-31 06:59:39 +0200

    feat: Display loader during place creation

--- client/src/features/create-edit-place/ui/PlaceEditor.tsx
+++ client/src/features/create-edit-place/ui/PlaceEditor.tsx
@@ -1,7 +1,9 @@
 import type { EventPlace } from "@/entities/place";
 import { type EventPlaceForm, PlaceForm } from "@/features/place-form";
+import { useSendTx } from "@/shared/hooks";
+import { Loader } from "@/shared/ui/Loader";
 import { useMutation } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useActiveAccount } from "thirdweb/react";
 import { upsertPlaceW3 } from "../api/upsertPlaceW3";
 import { type ModalStatus, type ModalType, PlaceDialog } from "./PlaceDialog";
@@ -17,10 +19,11 @@ export const PlaceEditor: React.FC<PlaceEditorProps> = ({ placeForEdit }) => {
     placeForEdit?.id ? "edit" : "create",
   );
   const account = useActiveAccount();
+  const { sendTx, error, isLoading } = useSendTx();
 
-  const { mutate, isPending } = useMutation({
+  const { mutate } = useMutation<unknown, Error, EventPlaceForm>({
     mutationFn: (values: EventPlaceForm) =>
-      upsertPlaceW3(values, account, placeForEdit?.id),
+      upsertPlaceW3(sendTx, values, account, placeForEdit?.id),
     onSuccess: () => {
       setStatus("success");
       setIsOpen(true);
@@ -31,13 +34,23 @@ export const PlaceEditor: React.FC<PlaceEditorProps> = ({ placeForEdit }) => {
     },
   });
 
+  // TODO: Get rid of `ModalStatus` and pass simply props
+  useEffect(() => {
+    if (error == null) return;
+    setStatus("error");
+  }, [error]);
+
   return (
     <div className="px-6">
-      <PlaceForm
-        existingPlace={placeForEdit}
-        disableFields={isPending}
-        onSubmit={mutate}
-      />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <PlaceForm
+          existingPlace={placeForEdit}
+          disableFields={isLoading}
+          onSubmit={mutate}
+        />
+      )}
       <PlaceDialog
         open={isOpen}
         setOpen={setIsOpen}
