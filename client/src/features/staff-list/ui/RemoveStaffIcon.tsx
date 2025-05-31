9afde95071cc204a5b3a810aaec43b0eedd0655d
scipunch <scipunch@gmail.com>
2025-05-31 06:39:55 +0200

    feat: Add loading state and error handling to remove staff icon

--- client/src/features/staff-list/ui/RemoveStaffIcon.tsx
+++ client/src/features/staff-list/ui/RemoveStaffIcon.tsx
@@ -1,4 +1,6 @@
+import { useSendTx } from "@/shared/hooks";
 import { removeStaff } from "@/shared/lib/web3";
+import { Loader } from "@/shared/ui/Loader";
 import { useActiveAccount } from "thirdweb/react";
 
 type RemoveStaffIconProps = {
@@ -9,19 +11,30 @@ export const RemoveStaffIcon: React.FC<RemoveStaffIconProps> = ({
   staffAddress,
 }) => {
   const account = useActiveAccount();
-
+  const { sendTx, error, isLoading } = useSendTx();
   async function deleteHandler() {
     if (!account) throw new Error("Account should be connected");
-    await removeStaff(account, staffAddress);
+    sendTx(removeStaff(account, staffAddress));
   }
-
+  if (error != null) {
+    alert("Failed to revoke staff role");
+  }
   return (
     <button
       className="h-full cursor-pointer"
       type="button"
       onClick={deleteHandler}
+      disabled={isLoading}
     >
-      <img className="h-full" src="/icons/staff bin_2.svg" alt="edit_button" />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <img
+          className="h-full"
+          src="/icons/staff bin_2.svg"
+          alt="edit_button"
+        />
+      )}
     </button>
   );
 };
