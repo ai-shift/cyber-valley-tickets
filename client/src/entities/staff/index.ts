267f23141c03e89302e9f886401e0b007936212e
scipunch <scipunch@gmail.com>
2025-06-02 16:09:45 +0200

    refactor: Move staff state to entities

---
+++ client/src/entities/staff/index.ts
@@ -0,0 +1,1 @@
+export { useStaffState, type StaffState } from "./model/slice";
---
+++ client/src/entities/staff/model/slice.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export type StaffState = {
+  removedStaff: string[];
+  optimisticRemoveStaff: (adr: string) => void;
+  optimisticAddStaff: (adr: string) => void;
+};
+
+export const useStaffState = create<StaffState>()(
+  persist(
+    (set, get) => ({
+      removedStaff: [],
+      optimisticRemoveStaff: (adr: string) =>
+        set({
+          removedStaff: [...get().removedStaff, adr.toLowerCase()],
+        }),
+      optimisticAddStaff: (adr: string) =>
+        set({
+          removedStaff: get().removedStaff.filter(
+            (staffAdr) => staffAdr !== adr.toLowerCase(),
+          ),
+        }),
+    }),
+    { name: "manageStaff" },
+  ),
+);
