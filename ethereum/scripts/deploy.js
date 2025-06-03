d4e9792e597efdbcdb30e6e234e05ba696e76ca7
scipunch <scipunch@gmail.com>
2025-06-03 12:01:42 +0200

    refactor: Use access control for event manager address

--- ethereum/scripts/deploy.js
+++ ethereum/scripts/deploy.js
@@ -1,9 +1,12 @@
 import EventManagerModule from "../ignition/modules/EventManager";
 import EventTicketModule from "../ignition/modules/EventTicket";
 import ERC20Module from "../ignition/modules/ERC20";
+import { keccak256 } from "@ethersproject/keccak256"
+import { toUtf8Bytes } from "@ethersproject/strings";;
 
 const MASTER_EOA = "0x2789023F36933E208675889869c7d3914A422921";
 const DEV_TEAM_EOA = MASTER_EOA;
+const getRole = (role) => keccak256(toUtf8Bytes(role));
 
 async function main() {
   const { erc20 } = await hre.ignition.deploy(ERC20Module, {});
@@ -18,7 +21,7 @@ async function main() {
       erc20: await erc20.getAddress()
     }}
   });
-  await eventTicket.setEventManagerAddress(await erc20.getAddress())
+  await eventTicket.grantRole(getRole("EVENT_MANAGER_ROLE"), await erc20.getAddress())
   console.log(`export PUBLIC_ERC20_ADDRESS=${await erc20.getAddress()}`)
   console.log(`export PUBLIC_EVENT_TICKET_ADDRESS=${await eventTicket.getAddress()}`)
   console.log(`export PUBLIC_EVENT_MANAGER_ADDRESS=${await eventManager.getAddress()}`)
