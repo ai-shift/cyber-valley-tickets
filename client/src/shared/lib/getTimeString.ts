809c11f48d495d7f778c43e493c08b713f605b17
Redmoor <danilamandrabura@gmail.com>
2025-06-26 15:34:28 +0300

    feat: Display event time in event details

--- client/src/shared/lib/getTimeString.ts
+++ client/src/shared/lib/getTimeString.ts
@@ -1,3 +1,14 @@
-export function getTimeString(date: Date) {
-  return `${date.getHours()}:${date.getMinutes() >= 10 ? date.getMinutes() : `0${date.getMinutes()}`}`;
+import { fromUnixTime } from "date-fns";
+
+export function getTimeString(date: Date | number) {
+  let actualDate: Date
+  if (date instanceof Date) {
+    actualDate = date;
+    return `${actualDate.getHours()}:${actualDate.getMinutes() >= 10 ? actualDate.getMinutes() : `0${actualDate.getMinutes()}`}`;
+  }
+  if (typeof date === "number") {
+    actualDate = fromUnixTime(date)
+        return `${actualDate.getHours()}:${actualDate.getMinutes() >= 10 ? actualDate.getMinutes() : `0${actualDate.getMinutes()}`}`;
+  }
+
 }
