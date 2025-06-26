c0e5174d46accbed507c5417ea2906ca73d26f5e
Redmoor <danilamandrabura@gmail.com>
2025-06-26 18:37:58 +0300

    feat: Add event chronology filters to account page

--- client/src/features/events-list/lib/filters.ts
+++ client/src/features/events-list/lib/filters.ts
@@ -1,5 +1,6 @@
 import type { Event } from "@/entities/event";
 import type { User } from "@/entities/user";
+import { getUnixTime } from "date-fns";
 
 export const uniteFilter = (event: Event, user: User) => {
   if (user.role === "master") return true;
@@ -7,14 +8,40 @@ export const uniteFilter = (event: Event, user: User) => {
   return event.status === "approved";
 };
 
-export const myEventsFilter = (event: Event, user: User) => {
+export type Chronology = "past" | "current" | "upcoming";
+export const myEventsFilter = (
+  event: Event,
+  user: User,
+  option: Chronology,
+) => {
+  const mapper: { [key in Chronology]: (event: Event) => boolean } = {
+    past: isPast,
+    current: isCurrent,
+    upcoming: isUpcoming,
+  };
+
   if (user.role === "master") {
-    return event.status === "submitted";
+    return event.status === "submitted" && mapper[option](event);
   }
   return (
     !!user.tickets.find((ticket) => ticket.eventId === event.id) ||
-    event.creator.address === user.address
+    (event.creator.address === user.address && mapper[option](event))
   );
+};
+
+const isPast = (event: Event) => {
+  return (
+    event.startDateTimestamp + event.daysAmount * 24 * 60 * 60 * 60 <
+    getUnixTime(new Date())
+  );
+};
+
+const isUpcoming = (event: Event) => {
+  return getUnixTime(new Date()) < event.startDateTimestamp;
+};
+
+const isCurrent = (event: Event) => {
+  return !isPast(event) && !isUpcoming(event);
 };
 
 export const upcomingFilter = (event: Event) => {
