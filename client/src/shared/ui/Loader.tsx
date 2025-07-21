f9638775c03efe6b54dea3d9714ce0d7a15d53e0
Redmoor <danilamandrabura@gmail.com>
2025-07-21 23:11:51 +0300

    refactor: Allow custom loader container styling

--- client/src/shared/ui/Loader.tsx
+++ client/src/shared/ui/Loader.tsx
@@ -1,12 +1,13 @@
 import { cn } from "../lib/utils";
 
 type LoaderProps = {
+  containerClassName?: string;
   className?: string;
 };
 
-export const Loader: React.FC<LoaderProps> = ({ className }) => {
+export const Loader: React.FC<LoaderProps> = ({ className, containerClassName }) => {
   return (
-    <div className="flex w-full aspect-square items-center justify-center">
+    <div className={cn("flex w-full aspect-square items-center justify-center", containerClassName)}>
       <div className={cn("aspect-square h-20", className)}>
         <div className="loader" />
       </div>
