1063d07b201d6b40a0f73992e299983ebc4d3e7b
scipunch <scipunch@gmail.com>
2025-07-21 12:13:47 +0200

    chore: Show result dialog on staff remove

--- client/src/features/staff-list/ui/RemoveStaffIcon.tsx
+++ client/src/features/staff-list/ui/RemoveStaffIcon.tsx
@@ -2,6 +2,8 @@ import { useStaffState } from "@/entities/staff";
 import { useSendTx } from "@/shared/hooks";
 import { removeStaff } from "@/shared/lib/web3";
 import { Loader } from "@/shared/ui/Loader";
+import { ResultDialog } from "@/shared/ui/ResultDialog";
+import { useState } from "react";
 import { useActiveAccount } from "thirdweb/react";
 
 type RemoveStaffIconProps = {
@@ -13,7 +15,8 @@ export const RemoveStaffIcon: React.FC<RemoveStaffIconProps> = ({
   staffAddress,
 }) => {
   const account = useActiveAccount();
-  const { sendTx, error, isLoading } = useSendTx();
+  const [isOpen, setIsOpen] = useState(false);
+  const { sendTx, error, isLoading, data: txHash } = useSendTx();
   const { optimisticRemoveStaff } = useStaffState();
   async function deleteHandler() {
     if (!account) throw new Error("Account should be connected");
@@ -27,21 +30,34 @@ export const RemoveStaffIcon: React.FC<RemoveStaffIconProps> = ({
     alert("Failed to revoke staff role");
   }
   return (
-    <button
-      className="h-full cursor-pointer"
-      type="button"
-      onClick={deleteHandler}
-      disabled={isLoading}
-    >
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <img
-          className="h-full"
-          src="/icons/staff bin_2.svg"
-          alt="edit_button"
-        />
-      )}
-    </button>
+    <>
+      <button
+        className="h-full cursor-pointer"
+        type="button"
+        onClick={deleteHandler}
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <img
+            className="h-full"
+            src="/icons/staff bin_2.svg"
+            alt="edit_button"
+          />
+        )}
+      </button>
+      <ResultDialog
+        open={isOpen}
+        setOpen={setIsOpen}
+        title="Transaction sent!"
+        body={"Staff role will be removed soon."}
+        onConfirm={() => {
+          setIsOpen(false);
+        }}
+        failure={error != null}
+        txHash={txHash as string}
+      />
+    </>
   );
 };
