0f8899b9a15dd791832065878af1fee6ed16342c
scipunch <scipunch@gmail.com>
2025-07-21 11:49:11 +0200

    chore: Actualize ticket deployment module

--- ethereum/ignition/modules/EventTicket.ts
+++ ethereum/ignition/modules/EventTicket.ts
@@ -1,12 +1,16 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-
 const EventTicketModule = buildModule("EventTicket", (m) => {
+  const IPFS_HOST = process.env.IPFS_PUBLIC_HOST;
+  if (IPFS_HOST == null || IPFS_HOST === "") {
+    throw new Error(`IPFS_PUBLIC_HOST env var is missing: ${IPFS_HOST}`);
+  }
   const masterAddress = m.getParameter("masterAddress")
   const eventTicket = m.contract("CyberValleyEventTicket", [
     "CyberValleyEventTicket",
     "CVET",
-    masterAddress
+    masterAddress,
+    IPFS_HOST
   ]);
   return { eventTicket };
 });
