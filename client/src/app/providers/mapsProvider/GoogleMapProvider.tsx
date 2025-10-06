6381ec4efa7785205409d87de394351030a45bfb
Redmoor <danilamandrabura@gmail.com>
2025-10-06 20:22:18 +0300

    Add: Map provider

--- client/src/app/providers/mapsProvider/GoogleMapProvider.tsx
+++ client/src/app/providers/mapsProvider/GoogleMapProvider.tsx
@@ -3,8 +3,9 @@ import { APIProvider } from "@vis.gl/react-google-maps";
 export const GoogleMapsProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-
-    return <APIProvider apiKey={import.meta.env.PUBLIC_GOOGLE_MAPS_API_KEY}>
+  return (
+    <APIProvider apiKey={import.meta.env.PUBLIC_GOOGLE_MAPS_API_KEY}>
       {children}
     </APIProvider>
+  );
 };
