d95e8d5f8774956229559369555f30192ac96a54
Redmoor <danilamandrabura@gmail.com>
2025-10-10 21:16:54 +0300

    Add: implement tabs and add forms

--- client/src/features/shaman-verification/UI/ShamanVerification.tsx
+++ client/src/features/shaman-verification/UI/ShamanVerification.tsx
@@ -1,5 +1,24 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/shared/ui/Tabs";
+import { CompanyForm } from "./CompanyForm";
+import { IndividualForm } from "./IndividualForm";
 
 export const ShamanVerification: React.FC = () => {
-  return <p>VErificatcsa</p>;
+  return (
+    <Tabs defaultValue="individual">
+      <TabsList className="py-3">
+        <TabsTrigger className="text-xl font-bold uppercase" activeClassName="underline underline-offset-2" value="individual">Individual</TabsTrigger>
+        <TabsTrigger className="text-xl font-bold uppercase" activeClassName="underline underline-offset-2" value="company">
+          Legal entity / company (PT/PT PMA)
+        </TabsTrigger>
+      </TabsList>
+      <div className="py-5 px-10">
+        <TabsContent value="individual">
+          <IndividualForm />
+        </TabsContent>
+        <TabsContent value="company">
+          <CompanyForm />
+        </TabsContent>
+      </div>
+    </Tabs>
+  );
 };
