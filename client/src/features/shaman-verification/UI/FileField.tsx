314bdc37f685e3ed7fb68807d1c588c6eefba8c7
Redmoor <danilamandrabura@gmail.com>
2025-10-10 22:02:48 +0300

    Add: format and add id to file field

--- client/src/features/shaman-verification/UI/FileField.tsx
+++ client/src/features/shaman-verification/UI/FileField.tsx
@@ -1,26 +1,30 @@
-import { Input } from "@/shared/ui/input"
+import { Input } from "@/shared/ui/input";
 
 type FileFieldProps = {
   label: string;
   file: File | null;
   setFile: (file: File | null) => void;
-}
+  id: string;
+};
 
-export const FileField: React.FC<FileFieldProps> = ({label, file, setFile}) => {
-  return(
-    <label htmlFor="ktp" className="text-xl mb-2 flex flex-col gap-1">
+export const FileField: React.FC<FileFieldProps> = ({
+  label,
+  file,
+  setFile,
+  id,
+}) => {
+  return (
+    <label htmlFor={id} className="text-xl mb-2 flex flex-col gap-1">
       {label}
-        <div className="border-2 border-input bg-input/10 py-1 px-2">
-          <p className="text-lg text-muted">
-            {file ? file.name : "Choose file"}
-          </p>
-        </div>
-        <Input
-          id="ktp"
-          type="file"
-          hidden
-          onChange={(e) => setFile(e.target.files?.[0] || null)}
-        />
-      </label>
-  )
-}
+      <div className="border-2 border-input bg-input/10 py-1 px-2">
+        <p className="text-lg text-muted">{file ? file.name : "Choose file"}</p>
+      </div>
+      <Input
+        id={id}
+        type="file"
+        hidden
+        onChange={(e) => setFile(e.target.files?.[0] || null)}
+      />
+    </label>
+  );
+};
