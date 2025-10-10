2589acb144aebec40cceb4b254a7f8e6d37ea60c
Redmoor <danilamandrabura@gmail.com>
2025-10-10 23:20:44 +0300

    Add: refactoring + add sheet

---
+++ client/src/features/map/data/data.ts
@@ -0,0 +1,21 @@
+import bpn_data from "@/data/geodata/bpn_data.json";
+import bridge from "@/data/geodata/bridge.json";
+import districts from "@/data/geodata/districts.json";
+import dots from "@/data/geodata/dots.json";
+import leasehold from "@/data/geodata/leasehold.json";
+import lots from "@/data/geodata/lots.json";
+import objects from "@/data/geodata/objects.json";
+import paths from "@/data/geodata/paths.json";
+import regions from "@/data/geodata/regions.json";
+
+export const geodata = {
+  "Bpn data": bpn_data,
+  Bridge: bridge,
+  Districts: districts,
+  Dots: dots,
+  Leasehold: leasehold,
+  Lots: lots,
+  Objects: objects,
+  Path: paths,
+  Regions: regions,
+};
---
+++ client/src/features/map/ui/Placemark.tsx
@@ -0,0 +1,45 @@
+import { AdvancedMarker } from "@vis.gl/react-google-maps";
+import type { Placemark as PlacemarkType } from "../model/types.ts"
+import { Polygon } from "./components/polygon.tsx";
+import { truncateColorString } from "../lib/colorTruncator.ts";
+import { Polyline } from "./components/polyline.tsx";
+
+
+type PlacemarkProps = {
+  placemark: PlacemarkType;
+  onClick: (placemark: PlacemarkType) => void;
+};
+
+export const Placemark: React.FC<PlacemarkProps> = ({ placemark, onClick }) => {
+  const clickHandler = () => onClick(placemark);
+  switch (placemark.type) {
+    case "point":
+      return (
+        <AdvancedMarker onClick={clickHandler} position={placemark.coordinates}>
+          <img
+            src={placemark.iconUrl}
+            alt={`${placemark.name} marker`}
+            width={32}
+            height={32}
+          />
+        </AdvancedMarker>
+      );
+    case "polygon":
+      return (
+        <Polygon
+          onClick={clickHandler}
+          paths={placemark.coordinates}
+          fillColor={truncateColorString(placemark.polygon_color)}
+          strokeColor={truncateColorString(placemark.line_color)}
+        />
+      );
+    case "line":
+      return (
+        <Polyline
+          onClick={clickHandler}
+          path={placemark.coordinates}
+          strokeColor={truncateColorString(placemark.line_color)}
+        />
+      );
+  }
+};
