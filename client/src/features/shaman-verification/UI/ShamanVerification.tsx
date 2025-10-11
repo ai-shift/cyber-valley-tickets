cf76f381c6234b17bac3fb4f362f44af368ccc5e
Redmoor <danilamandrabura@gmail.com>
2025-10-11 19:21:15 +0300

    Add: styling

--- client/src/features/shaman-verification/UI/ShamanVerification.tsx
+++ client/src/features/shaman-verification/UI/ShamanVerification.tsx
@@ -5,20 +5,20 @@ import { IndividualForm } from "./IndividualForm";
 export const ShamanVerification: React.FC = () => {
   return (
     <Tabs defaultValue="individual">
-      <TabsList className="py-3 flex-wrap">
+      <TabsList className="py-3">
         <TabsTrigger
-          className="text-xl font-bold uppercase"
-          activeClassName="underline underline-offset-2"
+          className="border-2 border-black/0 p-2 text-xl font-bold uppercase"
+          activeClassName=" border-primary"
           value="individual"
         >
           Individual
         </TabsTrigger>
         <TabsTrigger
-          className="text-xl font-bold uppercase"
-          activeClassName="underline underline-offset-2"
+          className="border-2 border-black/0 p-2 text-xl font-bold uppercase"
+          activeClassName="border-primary"
           value="company"
         >
-          Legal entity / company
+          Legal entity
         </TabsTrigger>
       </TabsList>
       <div className="py-5 px-10">
