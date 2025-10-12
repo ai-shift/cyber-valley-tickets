a4730cd5f0c487deb6a8c90b86253df7e2a1d168
Redmoor <danilamandrabura@gmail.com>
2025-10-12 15:11:49 +0300

    Add: display placemark thumb

--- client/src/features/map/lib/colorTruncator.ts
+++ client/src/features/map/lib/colorTruncator.ts
@@ -9,3 +9,16 @@ export function truncateColorString(kmlColor: string): string {
 
   return `#${r}${g}${b}`.toLowerCase();
 }
+
+export function swapBR(kmlColor: string): string {
+  if (!kmlColor || kmlColor.length !== 8) {
+    throw new Error("Invalid KML color format (expected 8 hex chars).");
+  }
+
+  const ops = kmlColor.substring(0,2);
+  const b = kmlColor.substring(2, 4);
+  const g = kmlColor.substring(4, 6);
+  const r = kmlColor.substring(6, 8);
+
+  return `${ops}${r}${g}${b}`
+}
---
+++ client/src/features/map/lib/getThumbUrl.ts
@@ -0,0 +1,15 @@
+import type { Placemark } from "../model/types.ts";
+import { swapBR } from "./colorTruncator.ts";
+
+const BASE_ICON_URL = "https://mt.googleapis.com/vt/icon/name=icons/onion/"
+
+export const getThumbUrl = (mark: Placemark) => {
+  switch (mark.type) {
+    case "point":
+      return mark.iconUrl;
+    case "polygon":
+      return `${BASE_ICON_URL}1492-wht-polygon-blank.png&filter=${swapBR(mark.line_color)}`;
+    case "line":
+      return `${BASE_ICON_URL}1491-wht-line-blank.png&filter=${swapBR(mark.line_color)}`;
+  }
+};
