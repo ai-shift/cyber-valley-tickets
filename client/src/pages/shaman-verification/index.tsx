4141521d5dc1ee8cea1b195ae1864db56ff26770
Redmoor <danilamandrabura@gmail.com>
2025-10-13 20:31:01 +0300

    Add: rename apparently

---
+++ client/src/pages/shaman-verification/index.tsx
@@ -0,0 +1,10 @@
+import { ShamanVerification } from "@/features/shaman-verification";
+import { PageContainer } from "@/shared/ui/PageContainer";
+
+export const ShamanVerificationPage: React.FC = () => {
+  return (
+    <PageContainer name="Shaman Verification">
+      <ShamanVerification />
+    </PageContainer>
+  );
+};
