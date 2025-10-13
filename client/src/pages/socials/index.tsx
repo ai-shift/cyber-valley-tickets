be8a40d630ac526b8e1399076ec3a4905d00c7ce
Redmoor <danilamandrabura@gmail.com>
2025-10-13 22:38:57 +0300

    Add: socials upsert logic

--- client/src/pages/socials/index.tsx
+++ client/src/pages/socials/index.tsx
@@ -1,19 +1,32 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+import { useAuthSlice } from "@/app/providers";
+import { upsertSocials, type Socials } from "@/entities/socials";
 import { SocialsForm } from "@/features/socials-form";
 import { PageContainer } from "@/shared/ui/PageContainer";
-import { Socials } from "@/entities/socials";
-import { useAuthSlice } from "@/app/providers";
 
 export const SocialsPage: React.FC = () => {
-  const {user} = useAuthSlice()
+  const { user } = useAuthSlice();
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
-  function handleSubmit(socials: Socials) {
-    // FIXME: ADD fetch call when ready
-    console.log(socials)
+  if (!user) return;
+
+  async function handleSubmit(socials: Socials) {
+    setError(false)
+    const {response} = await upsertSocials(socials);
+    if (!response.ok) {
+      setError(true)
+      console.error("Failed to set socials")
+    }
+    navigate(-1)
   }
 
   return (
     <PageContainer name="Socials">
-      <SocialsForm onSubmit={handleSubmit} existingSocials={user.socials} />
+      <SocialsForm onSubmit={handleSubmit} existingSocials={user.socials[0]} />
+      {error && <p className="text-center py-6 text-lg text-red-500">Some problem occured while setting socials. <br/> Try again.</p>}
     </PageContainer>
   );
 };
