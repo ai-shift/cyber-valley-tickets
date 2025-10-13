06d376d511e99962e6f31407d1708cfb781ec31b
Redmoor <danilamandrabura@gmail.com>
2025-10-13 20:35:27 +0300

    Add: local providers list and form copied from staff (fuck react)

---
+++ client/src/features/localprovider-form/model/formSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+  address: z
+    .string()
+    .length(42, "Address should be 42 symbols long")
+    .nonempty("Address is required")
+    .refine((val) => val.startsWith("0x"), {
+      message: "Address must start with '0x'",
+    }),
+  share: z
+    .number()
+    .int()
+    .gt(0, "Share have to be a whole number from 0 to 100")
+    .lt(100, "Share have to be a whole number from 0 to 100"),
+});
