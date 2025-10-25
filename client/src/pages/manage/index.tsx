f1505923b4d952ff65e30524fc46e5e6c7de79ef
Redmoor <danilamandrabura@gmail.com>
2025-10-25 19:26:38 +0300

    Refactor: enhance the manage page

--- client/src/pages/manage/index.tsx
+++ client/src/pages/manage/index.tsx
@@ -1,38 +1,57 @@
 import { useAuthSlice } from "@/app/providers";
+import type { User } from "@/entities/user";
 import { PageContainer } from "@/shared/ui/PageContainer";
 import { Link } from "react-router";
 
 export const ManagePage: React.FC = () => {
   const { user } = useAuthSlice();
 
+  if (!user) {
+    return null;
+  }
+
   return (
     <PageContainer hasBackIcon={false} name="Manage">
       <div className="flex flex-col gap-7 px-5 py-9">
-        {user?.role === "localprovider" && (
-          <>
-            <Link
-              className="card border-primary/30 text-center text-xl py-5"
-              to="/manage/place"
-            >
-              Manage places
-            </Link>
-            <Link
-              className="card border-primary/30 text-center text-xl py-5"
-              to="/manage/staff"
-            >
-              Manage staff
-            </Link>
-          </>
-        )}
-        {user?.role === "master" && (
-          <Link
-            className="card border-primary/30 text-center text-xl py-5"
-            to="/manage/localproviders"
-          >
-            Manage local providers
-          </Link>
-        )}
+        {manageView(user)}
       </div>
     </PageContainer>
   );
 };
+
+const manageView = (user: User): React.ReactNode => {
+  switch (user.role) {
+    case "master":
+      return <MasterView />
+    case "localprovider":
+      return <LocalProviderView />
+    default:
+      return null
+  }
+}
+
+const LocalProviderView = (): React.ReactNode => (
+  <>
+    <Link
+      className="card border-primary/30 text-center text-xl py-5"
+      to="/manage/place"
+    >
+      Manage places
+    </Link>
+    <Link
+      className="card border-primary/30 text-center text-xl py-5"
+      to="/manage/staff"
+    >
+      Manage staff
+    </Link>
+  </>
+);
+
+const MasterView = (): React.ReactNode=> (
+  <Link
+    className="card border-primary/30 text-center text-xl py-5"
+    to="/manage/localproviders"
+  >
+    Manage local providers
+  </Link>
+);
