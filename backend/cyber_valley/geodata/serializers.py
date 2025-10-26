b932359c7d08fdef25660e052b056add563c27c7
scipunch <scipunch@gmail.com>
2025-10-26 19:06:14 +0100

    backend: annotate mutable class attributes with ClassVar

--- backend/cyber_valley/geodata/serializers.py
+++ backend/cyber_valley/geodata/serializers.py
@@ -1,12 +1,14 @@
+from typing import Any, ClassVar
+
 from rest_framework import serializers
 
 from .models import GeodataLayer
 
 
-class GeodataLayerSerializer(serializers.ModelSerializer):
+class GeodataLayerSerializer(serializers.ModelSerializer[GeodataLayer]):
     class Meta:
         model = GeodataLayer
-        fields = ["data"]
+        fields: ClassVar[list[str]] = ["data"]
 
-    def to_representation(self, instance):
+    def to_representation(self, instance: GeodataLayer) -> Any:
         return instance.data
