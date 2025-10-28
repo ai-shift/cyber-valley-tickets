01f12e7ef6a876548b4e0d670b56dbcf889c63fa
Redmoor <danilamandrabura@gmail.com>
2025-10-28 18:31:06 +0200

    small fixes

--- client/src/features/place-form/ui/PlaceForm.tsx
+++ client/src/features/place-form/ui/PlaceForm.tsx
@@ -16,7 +16,7 @@ import {
 } from "@/shared/ui/form";
 import { Input } from "@/shared/ui/input";
 
-import { placesQueries, type EventPlace } from "@/entities/place";
+import { type EventPlace, placesQueries } from "@/entities/place";
 import { handleNumericInput } from "@/shared/lib/handleNumericInput";
 import { Switch } from "@/shared/ui/switch";
 import { usePlacePersist } from "../hooks/usePlacePersist";
@@ -24,10 +24,10 @@ import { formSchema } from "../model/formSchema";
 
 import type { LatLng } from "@/entities/geodata";
 import { EbaliMap } from "@/features/map";
+import { Circle } from "@/features/map";
+import { useQuery } from "@tanstack/react-query";
 import { AdvancedMarker, Pin } from "@vis.gl/react-google-maps";
 import { twMerge } from "tailwind-merge";
-import { useQuery } from "@tanstack/react-query";
-import { Circle } from "@/features/map";
 
 type PlaceFormProps = {
   existingPlace?: EventPlace;
@@ -40,7 +40,7 @@ export const PlaceForm: React.FC<PlaceFormProps> = ({
   onSubmit: submitHandler,
   disableFields,
 }) => {
-  const {data: places} = useQuery(placesQueries.list());
+  const { data: places } = useQuery(placesQueries.list());
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -125,12 +125,19 @@ export const PlaceForm: React.FC<PlaceFormProps> = ({
                           }
                     }
                   >
-                    {
-                      places?.map(place => {
-                        const isExisting = place.id === existingPlace?.id
-                      return <Circle key={place.id} center={place.geometry.coordinates[0]} radius={60} strokeWeight={2} strokeColor={isExisting ? "#3e8801" : "#02d6f2" } fillColor={isExisting ? "#3e8801" : "#02d6f2" } />
-                      })
-                    }
+                    {places?.map((place) => {
+                      const isExisting = place.id === existingPlace?.id;
+                      return (
+                        <Circle
+                          key={place.id}
+                          center={place.geometry.coordinates[0]}
+                          radius={60}
+                          strokeWeight={2}
+                          strokeColor={isExisting ? "#3e8801" : "#02d6f2"}
+                          fillColor={isExisting ? "#3e8801" : "#02d6f2"}
+                        />
+                      );
+                    })}
                     {selectedLocation && (
                       <AdvancedMarker position={selectedLocation}>
                         <Pin
