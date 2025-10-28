5cb8555426e1bc3de2f00bf9e478f5eb3c9cad45
scipunch <scipunch@gmail.com>
2025-10-28 02:28:30 +0100

    proj: update makefiles

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,13 +9,14 @@ export default defineConfig({
   plugins: [react(), tailwindcss(), nodePolyfills()],
   envPrefix: "PUBLIC_",
   server: {
+    port: Number(process.env.VITE_PORT),
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: `http://127.0.0.1:${process.env.BACKEND_PORT}`,
         changeOrigin: true,
       },
       "/ganache": {
-        target: "http://127.0.0.1:8545",
+        target: `http://127.0.0.1:${process.env.GANACHE_PORT}`,
         rewrite: (path) => path.replace(/^\/ganache/, ""),
         changeOrigin: true,
       },
