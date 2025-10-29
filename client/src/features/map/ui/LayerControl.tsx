875907159115ae7c66fa9b99586ae9dc66a5ae08
scipunch <scipunch@gmail.com>
2025-10-29 03:47:39 +0100

    client: make map layers list collapsible

--- client/src/features/map/ui/LayerControl.tsx
+++ client/src/features/map/ui/LayerControl.tsx
@@ -1,6 +1,10 @@
 import type { Placemark } from "@/entities/geodata";
 import { Loader } from "@/shared/ui/Loader";
+import { Expandable } from "@/shared/ui/expandable/ui/Expandable";
+import { ExpandableContent } from "@/shared/ui/expandable/ui/ExpandableContent";
+import { ExpandableTrigger } from "@/shared/ui/expandable/ui/ExpandableTrigger";
 import { useMap } from "@vis.gl/react-google-maps";
+import { twMerge } from "tailwind-merge";
 import { getPlacemarkPosition } from "../lib/getCenterPosition.ts";
 import { getThumbUrl } from "../lib/getThumbUrl.ts";
 
@@ -42,36 +46,51 @@ export const LayerControl: React.FC<LayerControlProps> = ({
   };
 
   return (
-    <div>
-      <label className="capitalize flex gap-3 text-xl">
-        <input
-          type="checkbox"
-          checked={isDisplayed}
-          onChange={() => setIsDisplayed(value)}
-        />
-        {value.replace(/_/, " ")}
-      </label>
+    <Expandable defaultOpened>
+      <ExpandableTrigger className="capitalize flex gap-3 text-xl items-center w-full">
+        {({ isCurrentExpanded }: { isCurrentExpanded: boolean }) => (
+          <>
+            <input
+              type="checkbox"
+              checked={isDisplayed}
+              onChange={() => setIsDisplayed(value)}
+              onClick={(e) => e.stopPropagation()}
+            />
+            <img
+              className={twMerge(
+                "h-4 transition-all duration-300",
+                isCurrentExpanded && "rotate-90",
+              )}
+              alt="chevrone"
+              src="/icons/chevrone_right.svg"
+            />
+            <span>{value.replace(/_/, " ")}</span>
+          </>
+        )}
+      </ExpandableTrigger>
       {isLoading && <Loader containerClassName="h-10" className="h-4" />}
       {isError && <p>Couldn't load the layer</p>}
       {isDisplayed && !isError && (
-        <div className="flex flex-col items-start">
-          {placemarks?.map((placemark, idx) => (
-            <button
-              type="button"
-              className="flex gap-1 items-center justify-center"
-              key={`${placemark.name}-${idx}`}
-              onClick={() => handleClick(placemark)}
-            >
-              <img
-                className="aspect-square h-6"
-                alt={placemark.name}
-                src={getThumbUrl(placemark)}
-              />
-              <p className="text-lg">{placemark.name}</p>
-            </button>
-          ))}
-        </div>
+        <ExpandableContent>
+          <div className="flex flex-col items-start">
+            {placemarks?.map((placemark, idx) => (
+              <button
+                type="button"
+                className="flex gap-1 items-center justify-center"
+                key={`${placemark.name}-${idx}`}
+                onClick={() => handleClick(placemark)}
+              >
+                <img
+                  className="aspect-square h-6"
+                  alt={placemark.name}
+                  src={getThumbUrl(placemark)}
+                />
+                <p className="text-lg">{placemark.name}</p>
+              </button>
+            ))}
+          </div>
+        </ExpandableContent>
       )}
-    </div>
+    </Expandable>
   );
 };
