5a07e78152481615e3e7fd9339cb804040021653
scipunch <scipunch@gmail.com>
2025-10-30 13:35:01 +0100

    client: prevent home map from scroll

--- client/src/pages/map/index.tsx
+++ client/src/pages/map/index.tsx
@@ -2,7 +2,7 @@ import { ApplyEventButton, HomeMap } from "@/features/home";
 
 export const MapPage = () => {
   return (
-    <div className="h-full flex flex-col relative">
+    <div className="h-full flex-1 flex-col relative overflow-hidden">
       <HomeMap className="flex-1 h-full" />
       <ApplyEventButton />
     </div>
