1a0e02fde7342df058272f0311ebd4d73ae414ac
Zhaqenohen <danilamandrabura@gmail.com>
2026-01-28 22:17:54 +0200

    map: remove redundant ApplyEventButton from map page layout

--- client/src/pages/map/index.tsx
+++ client/src/pages/map/index.tsx
@@ -1,10 +1,9 @@
-import { ApplyEventButton, HomeMap } from "@/features/home";
+import { HomeMap } from "@/features/home";
 
 export const MapPage = () => {
   return (
     <div className="h-full flex-1 flex-col relative overflow-y-hidden">
       <HomeMap />
-      <ApplyEventButton />
     </div>
   );
 };
