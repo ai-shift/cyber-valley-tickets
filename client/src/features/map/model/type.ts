04a4f260562d8dcfcd84861781ed9ce6fd2d6f60
Zhaqenohen <danilamandrabura@gmail.com>
2026-01-28 16:49:08 +0200

    code formatting

--- client/src/features/map/model/type.ts
+++ client/src/features/map/model/type.ts
@@ -1,6 +1,6 @@
-import { Event } from "@/entities/event"
-import { EventPlace } from "@/entities/place";
+import type { Event } from "@/entities/event";
+import type { EventPlace } from "@/entities/place";
 
-export interface PlaceWithEvents extends EventPlace  {
+export interface PlaceWithEvents extends EventPlace {
   events: Omit<Event, "place">[];
 }
--- client/src/features/map/ui/EventsLayerControl.tsx
+++ client/src/features/map/ui/EventsLayerControl.tsx
@@ -1,22 +1,18 @@
-import { Expandable } from "@/shared/ui/expandable/ui/Expandable"
-import { useMapState } from "../model/slice"
-import { ExpandableTrigger } from "@/shared/ui/expandable/ui/ExpandableTrigger"
-import { twMerge } from "tailwind-merge"
-import { ExpandableContent } from "@/shared/ui/expandable/ui/ExpandableContent"
+import { Expandable } from "@/shared/ui/expandable/ui/Expandable";
+import { ExpandableContent } from "@/shared/ui/expandable/ui/ExpandableContent";
+import { ExpandableTrigger } from "@/shared/ui/expandable/ui/ExpandableTrigger";
+import { twMerge } from "tailwind-merge";
+import { useMapState } from "../model/slice";
 
 export const EventsLayerControl = () => {
-  const {events, selectEventPlace} = useMapState()
+  const { events, selectEventPlace } = useMapState();
 
   return (
     <Expandable defaultOpened>
       <ExpandableTrigger className="capitalize flex gap-3 text-xl items-center w-full">
         {({ isCurrentExpanded }: { isCurrentExpanded: boolean }) => (
           <>
-            <input
-              type="checkbox"
-              readOnly
-              checked={true}
-            />
+            <input type="checkbox" readOnly checked={true} />
             <img
               className={twMerge(
                 "h-4 transition-all duration-300",
@@ -29,25 +25,25 @@ export const EventsLayerControl = () => {
           </>
         )}
       </ExpandableTrigger>
-        <ExpandableContent>
-          <div className="flex flex-col items-start">
-            {events.map((event) => (
-              <button
-                type="button"
-                className="flex gap-1 items-center justify-center"
-                key={event.id}
-                onClick={() => selectEventPlace(event.placeId)}
-              >
-                <img
-                  className="aspect-square h-6"
-                  alt={event.title}
-                  src={event.imageUrl ?? ""}
-                />
-                <p className="text-lg">{event.title}</p>
-              </button>
-            ))}
-          </div>
-        </ExpandableContent>
+      <ExpandableContent>
+        <div className="flex flex-col items-start">
+          {events.map((event) => (
+            <button
+              type="button"
+              className="flex gap-1 items-center justify-center"
+              key={event.id}
+              onClick={() => selectEventPlace(event.placeId)}
+            >
+              <img
+                className="aspect-square h-6"
+                alt={event.title}
+                src={event.imageUrl ?? ""}
+              />
+              <p className="text-lg">{event.title}</p>
+            </button>
+          ))}
+        </div>
+      </ExpandableContent>
     </Expandable>
-  )
-}
+  );
+};
