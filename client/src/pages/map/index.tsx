da504eb77cdbd4c4517039b95d64d90c128ff12e
Zhaqenohen <danilamandrabura@gmail.com>
2026-02-01 22:55:02 +0200

    feat: restructure map component for better layout and controls

--- client/src/pages/map/index.tsx
+++ client/src/pages/map/index.tsx
@@ -2,7 +2,7 @@ import { HomeMap } from "@/features/home";
 
 export const MapPage = () => {
   return (
-    <div className="h-full flex-1 flex-col relative overflow-y-hidden">
+    <div className="h-full flex-1 flex-col relative overflow-hidden">
       <HomeMap />
     </div>
   );
